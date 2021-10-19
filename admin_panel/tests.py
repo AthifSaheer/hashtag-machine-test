60813a0b9406c4e6709c921ab78d8b666cf14048
Athif Saheer <liteboook@gmail.com>
2021-10-19 13:15:19 +0530

    test cases completed in admin side

--- admin_panel/tests.py
+++ admin_panel/tests.py
@@ -1,3 +1,12 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .views import *
 
-# Create your tests here.
+class Test(TestCase):
+    def test_analytics(self):
+        url = reverse('analytics')
+        self.assertEqual(resolve(url).func, analytics)
+    
+    def test_ledger(self):
+        url = reverse('ledger')
+        self.assertEqual(resolve(url).func, ledger)
