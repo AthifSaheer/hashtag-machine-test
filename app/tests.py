e9640984f3c7c422eb6cdc7b0cfa05cc4e5d423a
Athif Saheer <liteboook@gmail.com>
2021-10-19 13:06:13 +0530

    test cases completed in user side

--- app/tests.py
+++ app/tests.py
@@ -1,15 +1,25 @@
-from django.test import TestCase
-from .views import register
 from django.urls import reverse, resolve
-def add_two_numbers(a, b):
-    return a + b
+from django.test import TestCase
+from .views import *
 
+class Test(TestCase):
+    def test_api_over_view(self):
+        url = reverse('api_over_view')
+        self.assertEqual(resolve(url).func, api_over_view)
 
-class TestExample(TestCase):
-
-    def test_add_two_numbers(self):
+    def test_register(self):
         url = reverse('register')
         self.assertEqual(resolve(url).func, register)
+    
+    def test_product_list(self):
+        url = reverse('product_list')
+        self.assertEqual(resolve(url).func, product_list)
+    
+    def test_order(self):
+        url = reverse('order')
+        self.assertEqual(resolve(url).func, order)
+    
+    def test_payment_transaction(self):
+        url = reverse('payment_transaction')
+        self.assertEqual(resolve(url).func, payment_transaction)
 
-    # def test_register(self):
-    #     self.assertIn('posts')
